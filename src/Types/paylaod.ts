002951219ca085e73faefd051f3e7243ddb42083
youssefelhoubi <elhoubiyoussef@gmail.com>
2026-09-23 17:01:26 +0000

    feat: update login method to return user payload and handle password validation

---
+++ src/Types/paylaod.ts
@@ -0,0 +1,5 @@
+export type Payload = {
+    userId: string;
+    email: string;
+    role: string;
+};
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ResourceNotFound } from 'src/exeptions/ResourceNotFound';
 import { UserDocument } from 'src/schemas/User.schema';
+import * as bcrypt from 'bcrypt';
+import { Payload } from 'src/Types/paylaod';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectModel('user') private readonly userModel: Model<UserDocument>
     ) { }
-    public async login(email: string, password: string): Promise<UserDocument | null> {
-        const user = await this.userModel.findOne({ email, password }).exec();
+
+    public async login(email: string, password: string): Promise<Payload | null> {
+        const user = await this.userModel.findOne({ email }).exec();
         if (!user) {
-            throw new 
+            throw new ResourceNotFound(`User with email ${email} not found or password is incorrect`, 404);
         }
-        return user;
+        const isValidated = await bcrypt.compare(password, user.passwordHash);
+        if (!isValidated) {
+            throw new ResourceNotFound(`User with email ${email} not found or password is incorrect`, 404);
+        }
+        const payload: Payload =  {
+            userId: user._id.toString(),
+            email: user.email,
+            role: user.role
+        };
+
+        return payload;
     }
 }
