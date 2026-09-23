bdca1e81faad50fb573c981aa13e33f71b729aa4
youssefelhoubi <elhoubiyoussef@gmail.com>
2026-09-23 18:10:49 +0000

    feat: remove unused AuthController and AuthService tests, streamline authentication methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,21 @@ import { LoginDto } from './dto/login.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/user/user.dto';
 
 @Injectable()
 export class AuthService {
     constructor(private readonly userService: UserService,
         private jtwService: JwtService
     ) { }
-    async login(loginDto: LoginDto) {
+    public async login(loginDto: LoginDto) {
         const result = await this.userService.login(loginDto.email, loginDto.password);
         const payload = { email: result?.email, sub: result.userId }
         return { token : this.jtwService.sign(payload) }
+    }
+    public async sighnUp(request: CreateUserDto) {
+        const result =  await this.userService.sighnUp(request);
+        const payload = { email: result.email, sub: result.userId };
+        return { token: this.jtwService.sign(payload) };
     }
 }
